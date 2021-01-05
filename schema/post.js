b88c50fdad82c16d0ffed38952ef87f23e512325
Ujjwol Kayastha <uzol123@gmail.com>
2021-01-05 20:54:48 +0545

    feat: update post schema

--- schema/post.js
+++ schema/post.js
@@ -2,7 +2,14 @@ const { gql } = require("apollo-server-express");
 
 //types query/mutation/subscription
 module.exports = gql`
+  type Post {
+    id: ID!
+    title: String!
+    description: String!
+  }
+
   type Query {
-    totalPosts: Int!
+    totalPosts: int!
+    allPosts: [Post!]!
   }
 `;
