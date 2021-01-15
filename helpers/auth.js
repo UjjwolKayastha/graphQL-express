f266e6bd9979eacbb2eae1015f4fff9351c91078
Ujjwol Kayastha <uzol123@gmail.com>
2021-01-15 13:20:58 +0545

    feat: implement firebase admin api to verify authorized user

--- helpers/auth.js
+++ helpers/auth.js
@@ -1,10 +1,31 @@
-let authorized = false;
+var admin = require("firebase-admin");
 
-exports.authCheck = (req, res, next = (f) => f) => {
-  //if nothing is passed next is a default function.
-  if (authorized) {
-    next();
-  } else {
-    throw new Error("Unauthorized");
+var serviceAccount = require("../config/firebaseServiceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+let authorized = true;
+
+exports.authCheck = async (req, res, next = (f) => f) => {
+  try {
+    const currentUser = await admin.auth().verifyIdToken(req.headers.authtoken);
+    console.log("CURRENT USER", currentUser);
+    return currentUser;
+  } catch (error) {
+    console.log("AUTH CHECK ERROR", error);
+    throw new Error("Invalid or expired token");
   }
+  //if nothing is passed next is a default function.
+  //   if (!req.headers.authtoken) throw new Error("Unauthorized");
+  //   const valid = req.headers.authtoken === "secret";
+  //   console.log("REQ", req);
+  //   console.log("HEADERS", req.headers);
+  //   console.log("AUTHTOKEN", req.headers.authtoken);
+  //   if (!valid) {
+  //     throw new Error("Unauthorized");
+  //   } else {
+  //     next();
+  //   }
 };
