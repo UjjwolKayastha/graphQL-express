d748ed7a3eb7ece3a8d72d288f1f4d520726c9d5
Ujjwol Kayastha <uzol123@gmail.com>
2021-01-26 21:25:52 +0545

    feat: add auth typedefs for user creation and update

--- typeDefs/auth.js
+++ typeDefs/auth.js
@@ -5,4 +5,43 @@ module.exports = gql`
   type Query {
     me: String!
   }
+  type UserCreateResponse {
+    username: String!
+    email: String!
+  }
+
+  # image input
+  input ImageInput {
+    url: String
+    public_id: String
+  }
+
+  type Image {
+    url: String
+    public: String
+  }
+
+  # returned user type
+  type User {
+    _id: ID!
+    username: String
+    name: String
+    email: String
+    images: [Image]
+    about: String
+    createdAt: String
+    updatedAt: String
+  }
+
+  input UserUpdateInput {
+    username: String
+    name: String
+    images: [ImageInput]
+    about: String
+  }
+
+  type Mutation {
+    userCreate: UserCreateResponse!
+    userUpdate(input: UserUpdateInput): User
+  }
 `;
