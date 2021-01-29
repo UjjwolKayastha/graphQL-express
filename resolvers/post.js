9c246f5975a877fc55897063eaba2cf8cfa49352
Ujjwol Kayastha <uzol123@gmail.com>
2021-01-29 17:57:44 +0545

    feat: remove auth check for all posts

--- resolvers/post.js
+++ resolvers/post.js
@@ -4,7 +4,6 @@ const { authCheck } = require("../helpers/auth");
 const totalPosts = () => posts.length;
 
 const allPosts = async (parent, args, { req }) => {
-  await authCheck(req);
   return posts;
 };
 
